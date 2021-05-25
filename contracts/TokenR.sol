9cda3e34565015563d6571b8c614a4d7a470f64c
Anupam Kumar <atiriktmail@gmail.com>
2021-05-25 18:25:04 +0530

    delegation definitions 2. add tranferFrom function and tests.
    Signed-off-by: Anupam Kumar <atiriktmail@gmail.com>

--- contracts/TokenR.sol
+++ contracts/TokenR.sol
@@ -41,5 +41,15 @@ contract TokenR{
 		return true;		
 	}
 	
+	function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+
+		require(balanceOf[_from]>=_value, "balance less of source account");
+		require(allowance[_from][msg.sender]>=_value, "transfer value higher than approved");
+		allowance[_from][msg.sender] -= _value;
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		emit Transfer(_from, _to, _value);
+		return true;
+	}
 
 }
--- test/TokenR.js
+++ test/TokenR.js
@@ -44,8 +44,8 @@ contract("TokenR", function(accounts){
       return tokenObj.transfer(accounts[1],250000, {from: accounts[0]});
     }).then(function(receipt){
       assert.equal(receipt.logs[0].args._from, accounts[0], 'source account ok');
-      assert.equal(receipt.logs[0].args._to, accounts[1], 'source account ok');
-      assert.equal(receipt.logs[0].args._value, 250000, 'source account ok');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'dest account ok');
+      assert.equal(receipt.logs[0].args._value, 250000, 'val account ok');
       
       return tokenObj.balanceOf(accounts[1]);
     }).then(function(balance){
@@ -56,7 +56,7 @@ contract("TokenR", function(accounts){
     });
   })
 
-  it('delegated transfer', function(){
+  it('sets up delegated transfer', function(){
     return TokenR.deployed().then(function(Obj){
       tokenObj =Obj;
       return tokenObj.approve.call(accounts[1], 100);
@@ -65,24 +65,64 @@ contract("TokenR", function(accounts){
       return tokenObj.approve(accounts[1], 100,{from:accounts[0]});
     }).then(function(receipt){
       assert.equal(receipt.logs[0].args._owner, accounts[0], 'source account ok');
-      assert.equal(receipt.logs[0].args._spender, accounts[1], 'source account ok');
-      assert.equal(receipt.logs[0].args._value, 100, 'source account ok');
+      assert.equal(receipt.logs[0].args._spender, accounts[1], 'dest account ok');
+      assert.equal(receipt.logs[0].args._value, 100, 'val account ok');
       return tokenObj.allowance(accounts[0], accounts[1]);
     }).then(function(_value){
       assert.equal(_value.toNumber(), 100, 'value ok');
     })
   })
 
-
-
-
-
-
-
-
-
-
-
+  it('handles delegated transfer', function(){
+    return TokenR.deployed().then(function(Obj){
+      tokenObj = Obj;
+      from_ =accounts[5];
+      to = accounts[6];
+      delegated = accounts[7];
+      return tokenObj.transfer(from_, 1000, {from:accounts[0]});
+    }).then(function(receipt){
+      return tokenObj.approve(delegated, 100, {from:from_});
+    }).then(function(receipt){
+      return tokenObj.transferFrom(from_, to, 20, {from:delegated});
+    }).then(function(receipt){
+      return tokenObj.balanceOf(to);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 20, 'delegated ok');
+      return (tokenObj.allowance(from_, delegated)); 
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 80, 'left allowance ok');
+      return tokenObj.balanceOf(from_);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 980, 'left source balance ok');
+      return tokenObj.transferFrom(from_, to, 400, {from:delegated});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf('revert') >= 0,'cannot transfer value larger than allowance');
+      return tokenObj.approve(delegated, 10000, {from:from_});
+    }).then(function(receipt){
+      return tokenObj.allowance(from_, delegated);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 10000, 'additional approval ok');
+      return tokenObj.transferFrom(from_, to, 5000, {from:delegated});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf('revert') >= 0, "cannot transfer more than source balance");
+      return tokenObj.transferFrom(from_, to, 500, {from:delegated});
+    }).then(function(receipt){
+      return tokenObj.balanceOf(to);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 520, 'readd ok');
+      return tokenObj.allowance(from_, delegated);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 9500, 'allowance ok after readd');
+      return tokenObj.balanceOf(from_);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 480, 'source balance ok after readd');
+      return tokenObj.transferFrom(from_, to, 10, {from:delegated});
+    }).then(function(receipt){
+      assert.equal(receipt.logs[0].args._from, from_, 'source account ok');
+      assert.equal(receipt.logs[0].args._to, to, 'dest account ok');
+      assert.equal(receipt.logs[0].args._value, 10, 'val account ok');
+    })
+  });
 
 
   });
